81f145739cc025bdac17434b3aec7097db648d8a
ScorpioWen <anyingtianxie@gmail.com>
2018-03-27 15:52:08 +0800

    20180327

---
+++ js/pswhide.js
@@ -0,0 +1,16 @@
+
+function hide(){
+    
+    var psw = document.getElementById('psw');
+    if(psw.getAttribute('type') == 'text'){
+        psw.setAttribute('type','password')    
+    }else{
+        psw.setAttribute('type','text')    
+    }
+}
+
+
+
+function enter(){
+    var user
+}
